8cf1cf23c8001b6a4fceb65a1c1bd6fea4df21e4
Tia Gong <gongtiw@gmail.com>
2026-05-18 17:25:25 -0700

    update

--- static/js/main.js
+++ static/js/main.js
@@ -26,8 +26,8 @@ var n_second=new Array;
 
 const exp_start_time=Date.now();
 
-// const trial_total=36;
-const trial_total=1;
+const trial_total=36;
+// const trial_total=1;
 const trial_list=shuffle([0,1,2,3,4,5,6,7,8,9,10,11,12,
 													13,14,15,16,17,18,19,20,21,22,23,24,
 													25,26,27,28,29,30,31,32,33,34,35])
